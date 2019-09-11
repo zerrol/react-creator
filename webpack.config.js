364c4695cad1c32f08c2bc1a546b2c31d7974496
FRozen <351988262@qq.com>
2019-09-11 10:48:09 +0800

    优化weback配置文件结构

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
-const commonConfig = require('./webpack/common')
+const commonConfig = require('./webpack/common.config')
 const merge = require('webpack-merge')
 
 // 开发环境配置
-let config = require('./webpack/dev')
+let config = require('./webpack/dev.config')
 
 // 生产环境配置
 if(process.env.NODE_ENV === 'production') {
-  config = require('./webpack/prod')
+  config = require('./webpack/prod.config')
 }
 
 config = merge.smart(commonConfig, config)
