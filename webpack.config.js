785f5c159e8096696362baa29d7d8f87117c5fba
FRozen <351988262@qq.com>
2019-09-23 10:31:04 +0800

    add webpack speed measure

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const commonConfig = require('./webpack/common.config')
 const merge = require('webpack-merge')
+const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+const smp = new SpeedMeasurePlugin();
 
 // 开发环境配置
 let config = require('./webpack/dev.config')
@@ -11,4 +13,4 @@ if(process.env.NODE_ENV === 'production') {
 
 config = merge.smart(commonConfig, config)
 // console.log(config.module.rules)
-module.exports = config
+module.exports = smp.wrap(config)
