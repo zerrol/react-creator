3b07e4c848d8f9eb4772f8998ea04f8dc03335e7
qi_yan <351988262@qq.com>
2020-08-16 18:36:29 +0800

    teser

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -1,6 +1,7 @@
 
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserWebpackPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   // webpack 4.x 默认使用的 uglyJsPlugin 进行压缩效率较低，可以使用 terser 进行优化
@@ -9,6 +10,7 @@ module.exports = {
   plugins: [
     // 每次打包前清除dist
     new CleanWebpackPlugin(),
+    new TerserWebpackPlugin()
     // 分离css文件
     new MiniCssExtractPlugin({
       filename: 'css/[name].[contenthash].css',
